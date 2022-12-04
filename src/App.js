926ecd56f629d48fef06a69719faeea8a6da4ffa
Suraj Pun <s_pun@outlook.com>
2022-12-04 15:41:07 +0000

    Data added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { useState, useEffect } from "react";
+
 function App() {
+  const [solution, setSolution] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/solutions")
+      .then((res) => res.json())
+      .then((json) => {
+        // Get random int between 0 & 14
+        const randomSolution = json[Math.floor(Math.random() * json.length)];
+
+        // Update State
+        setSolution(randomSolution.word);
+      });
+  }, [setSolution]);
+
   return (
     <div className="App">
       <h1>Wordle</h1>
+
+      {solution && <div>Solution is: {solution}</div>}
     </div>
   );
 }
 
 export default App;
+
+// To Run Server 3001 Api
+// npx json-server ./data/db.json --port 3001
